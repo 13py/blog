2de1f530c82201660879d2cad8632deeb5d2903e
Art <pytagpro@gmail.com>
2021-10-25 14:54:39 +0500

    Add new models Post in app_blog/models.py

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('Заголовок', max_length=255, db_index=True)
+    slug = models.SlugField('Читаемый url', max_length=255, unique=True)
+    text = models.TextField('Текст поста', blank=True, db_index=True)
+    date_pub_at = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
