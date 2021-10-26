eca52dd96d0a8ce116961fc81da970a9d3b5c7ca
Art <pytagpro@gmail.com>
2021-10-26 12:14:51 +0500

    Add new model Tag in app_blog

--- app_blog/models.py
+++ app_blog/models.py
@@ -13,3 +13,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.title
