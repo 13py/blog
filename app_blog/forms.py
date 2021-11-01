76e4e1a12576218431f07782f5b92a437a782179
Art <pytagpro@gmail.com>
2021-11-01 11:21:36 +0500

    Edit TagForm: change inherit TagForm(forms.ModelForm)

--- app_blog/forms.py
+++ app_blog/forms.py
@@ -4,12 +4,20 @@ from django.core.exceptions import ValidationError
 from app_blog.models import Tag
 
 
-class TagForm(forms.Form):
-    title = forms.CharField(label='Название тега', max_length=55)
-    slug = forms.SlugField(label='Слаг тега', max_length=55)
-
-    title.widget.attrs.update({'class': 'form-control'})
-    slug.widget.attrs.update({'class': 'form-control'})
+class TagForm(forms.ModelForm):
+    # title = forms.CharField(label='Название тега', max_length=55)
+    # slug = forms.SlugField(label='Слаг тега', max_length=55)
+    #
+    # title.widget.attrs.update({'class': 'form-control'})
+    # slug.widget.attrs.update({'class': 'form-control'})
+    class Meta:
+        model = Tag
+        fields = ('title', 'slug')
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
     def clean_slug(self):
         new_slug = self.cleaned_data.get('slug').lower()
@@ -22,9 +30,9 @@ class TagForm(forms.Form):
                                   f'Придумайте другой вариант!')
         return new_slug
 
-    def save(self):
-        new_tag = Tag.objects.create(
-            title=self.cleaned_data.get('title'),
-            slug=self.cleaned_data.get('slug'),
-        )
-        return new_tag
+    # def save(self):
+    #     new_tag = Tag.objects.create(
+    #         title=self.cleaned_data.get('title'),
+    #         slug=self.cleaned_data.get('slug'),
+    #     )
+    #     return new_tag
